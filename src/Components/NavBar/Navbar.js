5e75ac8833c26945fdf91ef423c9bb47a3bc9246
Vishnu M <vishnuvmk029@gmail.com>
2023-08-13 16:21:36 +0530

    navbar schema complete

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,21 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './image/logo.png';
 import './NavBar.css'
+import { CgSearch } from 'react-icons/cg';
+import { BsList } from 'react-icons/bs';
+import { BiSolidDownArrow } from 'react-icons/bi';
 
 function Navbar() {
   return (
     <div className='navbar'>
         <img src={logo} className="brand-logo" alt="" />
         <div className='input'>
-            <ion-icon name="search"></ion-icon>
+            <CgSearch className='search-icon'/>
             <input className='search' placeholder='Search for Movies, Events, Plays, Sports, and Activities'></input>
         </div>
         <div className="right-container">
-            <a href="#" className="location">Kollam<ion-icon name="caret-down-sharp"></ion-icon></a>
+            <a href="#" className="location">Kollam</a><BiSolidDownArrow className='location-down'/>
             <button className="signin">Sign in</button>
-
             <a href="#" className="btn-customized open-menu">
-                <ion-icon name="menu-outline"></ion-icon>
+              <BsList className="menu"/>
             </a>
         </div>
     </div>
