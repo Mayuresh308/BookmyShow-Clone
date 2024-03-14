6264d74cf2faffbe42e4f98b7b56832c6164d95e
Vishnu M <vishnuvmk029@gmail.com>
2024-03-14 22:01:39 +0530

    banner layer done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './Components/NavBar/Navbar';
 import SubNavBar from './Components/SubNavBar/SubNavBar';
+import Banner from './Components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <SubNavBar/>
+      <Banner />
     </div>
   );
 }
